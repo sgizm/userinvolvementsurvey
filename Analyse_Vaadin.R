209b6c9701a964a6890ca1e60e9184289b50351a
sgizm <musahedat1@gmail.com>
2017-04-10 17:26:05 +0300

    Vaadin 2.3

--- Analyse_Vaadin.R
+++ Analyse_Vaadin.R
@@ -84,6 +84,45 @@ data$useractivities.other <- data$Other
 data$useractivities.other.open <- data$If.other..please.specify..separate.with.commas.
 useractivities.options <- c("Forming ideas", "Gathering requirements", "Software design", "Implementing software", "Testing", "The activities after release", "Committing code", "Providing fixes", "Submitting bugs", "Participating in online discussion", "Other")
 
+#Other roles
+data$useractivities.other <- data$If.other..please.specify
+
+# 2.2. How much do you agree with the following statements? (User involvement statements)
+data$userinv.S1 <- data$X2.2..How.much.do.you.agree.with.the.following.statements...I.know.who.uses.the.software.I.contribute.to.in.my.work
+data$userinv.S2 <- data$X2.2..How.much.do.you.agree.with.the.following.statements...I.need.to.ask.for.permission.to.contact.users
+data$userinv.S3 <- data$X2.2..How.much.do.you.agree.with.the.following.statements...I.frequently.have.direct.contact.with.users
+data$userinv.S4 <- data$X2.2..How.much.do.you.agree.with.the.following.statements...I.have.sufficient.information.about.users....needs
+data$userinv.S5 <- data$X2.2..How.much.do.you.agree.with.the.following.statements...I.have.information.about.users.that.is.relevant.for.my.work
+data$userinv.S6 <- data$X2.2..How.much.do.you.agree.with.the.following.statements...The.information.I.have.about.users.is.up.to.date
+data$userinv.S7 <- data$X2.2..How.much.do.you.agree.with.the.following.statements...I.would.like.to.get.more.feedback.from.users
+
+userinv.statements <- c(
+  "I know who uses the software I contribute to in my work",
+  "I need to ask for permission to contact users",
+  "I frequently have direct contact with users",
+  "I have sufficient information about users' needs",
+  "I have information about users that is relevant for my work",
+  "The information I have about users is up to date",
+  "I would like to get more feedback from users"
+)
+userinv.options <- c("Completely disagree", "Disagree", "Neither disagree or agree", "Agree", "Completely agree", "I don't know")
+
+# 2.3 In your experience, how easy is it for the following to get information from users?
+data$userinf.pro.mng <- data$X2.3.In.your.experience..how.easy.is.it.for.the.following.roles.to.get.information.from.users...Product.managers
+data$userinf.mng <- data$X2.3.In.your.experience..how.easy.is.it.for.the.following.roles.to.get.information.from.users...Managers..other
+data$userinf.ux.designer <- data$X2.3.In.your.experience..how.easy.is.it.for.the.following.roles.to.get.information.from.users...UX.designers
+data$userinf.dev.frame <- data$X2.3.In.your.experience..how.easy.is.it.for.the.following.roles.to.get.information.from.users...Vaadin.Framework.developers
+data$userinf.dev.tool <- data$X2.3.In.your.experience..how.easy.is.it.for.the.following.roles.to.get.information.from.users...Vaadin.Pro.Tools.developers
+data$userinf.arc <- data$X2.3.In.your.experience..how.easy.is.it.for.the.following.roles.to.get.information.from.users...Software.architects.
+data$userinf.adv <- data$X2.3.In.your.experience..how.easy.is.it.for.the.following.roles.to.get.information.from.users...Product.and.service.advocates
+data$userinf.con <- data$X2.3.In.your.experience..how.easy.is.it.for.the.following.roles.to.get.information.from.users...Consultants
+data$userinf.cust.sup <- data$X2.3.In.your.experience..how.easy.is.it.for.the.following.roles.to.get.information.from.users...Customer.support
+data$userinf.tra <- data$X2.3.In.your.experience..how.easy.is.it.for.the.following.roles.to.get.information.from.users...Trainers
+data$userinf.slf <- data$X2.3.In.your.experience..how.easy.is.it.for.the.following.roles.to.get.information.from.users...Myself
+userinf.statements <- c("Product managers", "Managers, other", "UX designers", "Vaadin Framework developers", "Vaadin Pro Tools developers", "Software architects", "Product and service advocates", "Consultants", "Customer support", "Trainers", "Myself")
+userinf.options <- c("Very difficult", "Difficult", "Neither easy nor difficult", "Easy", "Very easy", "I don't know")
+
+
 attach(data)
 
 ######################################################################
@@ -181,3 +220,34 @@ print("Frequencies of development activities that users are involved in")
 summary(useractivities)
 ggplot(data=useractivities, aes(x=Activity, y=Frequency)) + labs(x="Development activities where users are involved") +
   geom_bar(stat="identity", fill="plum4", colour="black") + theme(axis.text=element_text(size=14))+ scale_x_discrete(limits=c("Forming ideas","Gathering requirements","Software design", "Implementing software", "Testing", "The activities after release", "Committing code", "Providing fixes", "Submitting bugs", "Participating in online discussion", "Other")) + scale_y_continuous(breaks=c(0,3,5,8,9,12,13,16,17), labels = c("0", "3", "5", "8", "9", "12", "13", "16", "17"))
+
+# 2.2 How much do you agree with the following statements?
+userinv <- data.frame(Statement=factor(rep(userinv.statements, each=length(userinv.S1))),
+                      Rating=c(
+                        userinv.S1,
+                        userinv.S2,
+                        userinv.S3,
+                        userinv.S4,
+                        userinv.S5,
+                        userinv.S6,
+                        userinv.S7))
+ggplot(data=userinv, aes(x=Statement, y=Rating, fill=Statement)) +
+  geom_boxplot() + guides(fill=FALSE) + coord_flip() + theme(axis.text=element_text(size=16))
+# + scale_y_discrete(name="Rating", limits=c("1","2", "3","4","5"))
+
+# 2.3 In your experience, how easy is it for the following to get information from users?
+userinf <- data.frame(Statement=factor(rep(userinf.statements, each=length(userinf.pro.mng))),
+                      Rating=c(
+                        userinf.pro.mng,
+                        userinf.mng,
+                        userinf.ux.designer,
+                        userinf.dev.frame,
+                        userinf.dev.tool,
+                        userinf.arc,
+                        userinf.adv,
+                        userinf.con,
+                        userinf.cust.sup,
+                        userinf.tra,
+                        userinf.slf))
+ggplot(data=userinf, aes(x=Statement, y=Rating, fill=Statement)) +
+  geom_boxplot() + guides(fill=FALSE) + coord_flip() + scale_x_discrete(limits=c("Myself", "Trainers", "Customer support", "Consultants", "Product and service advocates", "Software architects", "Vaadin Pro Tools developers", "Vaadin Framework developers", "UX designers", "Managers, other", "Product managers")) + theme(axis.text=element_text(size=16))
