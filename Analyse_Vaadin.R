f3de039341a8ef327f8da153bdc5e86783ebd6ef
sgizm <musahedat1@gmail.com>
2017-04-10 19:31:17 +0300

    End of desc. analysis

--- Analyse_Vaadin.R
+++ Analyse_Vaadin.R
@@ -122,6 +122,87 @@ data$userinf.slf <- data$X2.3.In.your.experience..how.easy.is.it.for.the.followi
 userinf.statements <- c("Product managers", "Managers, other", "UX designers", "Vaadin Framework developers", "Vaadin Pro Tools developers", "Software architects", "Product and service advocates", "Consultants", "Customer support", "Trainers", "Myself")
 userinf.options <- c("Very difficult", "Difficult", "Neither easy nor difficult", "Easy", "Very easy", "I don't know")
 
+# 2.4 How often do you use the following ways to get information about users?
+data$infofreq.O1 <- data$X2.4.How.often.do.you.use.the.following.ways.to.get.information.about.users...I.remotely.observe.users.when.they.are.using.the.software..e.g...screen.sharing.
+data$infofreq.O2 <- data$X2.4.How.often.do.you.use.the.following.ways.to.get.information.about.users...I.am.physically.present.with.users.when.they.are.using.the.software..e.g...talk.aloud.study.
+data$infofreq.O3 <- data$X2.4.How.often.do.you.use.the.following.ways.to.get.information.about.users...I.interact.with.the.users.before.they.use.the.software
+data$infofreq.O4 <- data$X2.4.How.often.do.you.use.the.following.ways.to.get.information.about.users...I.interact.with.users.in.person.after.they.used.the.software..e.g...post.use.interview.
+data$infofreq.O5 <- data$X2.4.How.often.do.you.use.the.following.ways.to.get.information.about.users...Through.recorded.usage.data..e.g...log.data.
+data$infofreq.O6 <- data$X2.4.How.often.do.you.use.the.following.ways.to.get.information.about.users...Through.online.discussion.channels..e.g..forums.
+
+infofreq.statements <- c(
+  "I remotely observe users when they are using the software (e.g., screen sharing)",
+  "I am physically present with users when they are using the software (e.g., talk-aloud study)",
+  "I interact with the users before they use the software",
+  "I interact with users in person after they used the software (e.g., post-use interview)",
+  "Through recorded usage data (e.g., log data)",
+  "Through online discussion channels")
+infofreq.options <- c("Never", "Rarely", "Sometimes", "Often", "Always")
+
+# 2.5 Try to remember a situation where you knew that involving users in development would be useful, but you could not involve them. Please describe the situation and what challenges you faced.
+data$userinf.open <- data$X2.5.Try.to.remember.a.situation.where.you.knew.that.involving.users.in.development.would.be.useful..but.you.could.not.involve.them..Please.describe.the.situation.and.what.challenges.you.faced.
+
+# 3.1 Does your company conduct experiments involving the users?
+data$condexp <- factor(data$X3.1.Does.your.company.conduct.experiments.involving.the.users.,
+                       levels = c(1:4, "NA"),
+                       labels = c("Never", "Rarely", "Occasionally", "Yes, actively", "I don't know"))
+
+# 3.2 Please describe a typical experiment you have seen or been involved in in your company, including the roles. (Skip this if you have not seen or been involved in any experiments.)
+data$exp.open <- data$X3.2.Please.describe.a.typical.experiment.you.have.seen.or.been.involved.in.in.your.company..including.the.roles...Skip.this.if.you.have.not.seen.or.been.involved.in.any.experiments..
+
+# 3.3 Below are three pairs of statements about collecting data for understanding user needs. How much do you agree with each statement?
+data$understanding.S1 <- data$To.understand.users..needs.better.......data.should.always.be.collected.because.it.might.be.needed.later
+data$understanding.S2 <- data$To.understand.users..needs.better.......data.should.only.be.collected.when.there.is.a.known.need.or.assumption
+data$understanding.S3 <- data$To.understand.users..needs.better.......rich..detailed.data.about.what.users.do.is.useful
+data$understanding.S4 <- data$To.understand.users..needs.better.......focused.data.on.a.specific.user.action.or.behaviour.is.useful
+data$understanding.S5 <- data$To.understand.users..needs.better.......users.themselves.must.be.actively.involved.in.shaping.the.software
+data$understanding.S6 <- data$To.understand.users..needs.better.......we.need.to.measure.user.behaviour.to.decide.what.the.software.should.be.like
+understanding.statements <- c(
+  "..data should always be collected because it might be needed later",
+  "..data should only be collected when there is a known need or assumption",
+  "..rich, detailed data about what users do is useful",
+  "..focused data on a specific user action or behaviour is useful",
+  "..users themselves must be actively involved in shaping the software",
+  "..we need to measure user behaviour to decide what the software should be like"
+)
+understanding.options <- c("Completely disagree", "Disagree", "Neither disagree or agree", "Agree", "Completely agree", "I don't know")
+
+# 4.1 How much do you agree with the following statements regarding notifying users about experiments? Please answer according to your personal beliefs.
+data$usernotif.S1 <- data$X4.1.How.much.do.you.agree.with.the.following.statements.regarding.notifying.users.about.experiments..Please.answer.according.to.your.personal.beliefs...Users.do.not.need.to.know.they.are.involved
+data$usernotif.S2 <- data$X4.1.How.much.do.you.agree.with.the.following.statements.regarding.notifying.users.about.experiments..Please.answer.according.to.your.personal.beliefs...If.we.collect.personal.information..users.need.to.be.notified
+data$usernotif.S3 <- data$X4.1.How.much.do.you.agree.with.the.following.statements.regarding.notifying.users.about.experiments..Please.answer.according.to.your.personal.beliefs...If.no.laws.are.being.broken..users.do.not.need.to.be.notified
+data$usernotif.S4 <- data$X4.1.How.much.do.you.agree.with.the.following.statements.regarding.notifying.users.about.experiments..Please.answer.according.to.your.personal.beliefs...Users.can.be.involved.in.an.experiment.without.their.knowledge.if.we.let.them.know.afterwards
+data$usernotif.S5 <- data$X4.1.How.much.do.you.agree.with.the.following.statements.regarding.notifying.users.about.experiments..Please.answer.according.to.your.personal.beliefs...Users.should.always.be.notified.when.they.are.being.involved.in.an.experiment
+data$usernotif.S6 <- data$X4.1.How.much.do.you.agree.with.the.following.statements.regarding.notifying.users.about.experiments..Please.answer.according.to.your.personal.beliefs...It.is.ok.not.to.disclose.all.the.experiment.details.to.users.involved
+data$usernotif.S7 <- data$X4.1.How.much.do.you.agree.with.the.following.statements.regarding.notifying.users.about.experiments..Please.answer.according.to.your.personal.beliefs...It.is.ok.to.trick.the.user.if.the.validity.of.experiment.results.depend.on.it
+usernotif.statements <- c(
+  "Users do not need to know they are involved",
+  "If we collect personal information, users need to be notified",
+  "If no laws are being broken, users do not need to be notified",
+  "Users can be involved in an experiment without their knowledge if we let them know afterwards",
+  "Users should always be notified when they are being involved in an experiment",
+  "It is ok not to disclose all the experiment details to users involved",
+  "It is ok to trick the user if the validty of experiment results depend on it")
+usernotif.options <- c("Completely disagree", "Disagree", "Neither disagree or agree", "Agree", "Completely agree", "I don't know")
+
+# 4.2 How much do you agree with the following statements about involving users in experiments? Please answer according to your personal beliefs.
+data$expinv.S1 <- data$X4.2.How.much.do.you.agree.with.the.following.statements.about.involving.users.in.experiments..Please.answer.according.to.your.personal.beliefs...I.cannot.trust.that.the.results.will.be.correct
+data$expinv.S2 <- data$X4.2.How.much.do.you.agree.with.the.following.statements.about.involving.users.in.experiments..Please.answer.according.to.your.personal.beliefs...Involving.users.in.experiments.is.time.consuming
+data$expinv.S3 <- data$X4.2.How.much.do.you.agree.with.the.following.statements.about.involving.users.in.experiments..Please.answer.according.to.your.personal.beliefs...Our.company.does.not.have.the.needed.technical.infrastructure
+data$expinv.S4 <- data$X4.2.How.much.do.you.agree.with.the.following.statements.about.involving.users.in.experiments..Please.answer.according.to.your.personal.beliefs...Users.would.not.like.to.be.part.of.software.experiments
+data$expinv.S5 <- data$X4.2.How.much.do.you.agree.with.the.following.statements.about.involving.users.in.experiments..Please.answer.according.to.your.personal.beliefs...Users.have.to.be.convinced.of.the.benefit.before.taking.part
+data$expinv.S6 <- data$X4.2.How.much.do.you.agree.with.the.following.statements.about.involving.users.in.experiments..Please.answer.according.to.your.personal.beliefs...Experiments.give.users.false.expectations
+data$expinv.S7 <- data$X4.2.How.much.do.you.agree.with.the.following.statements.about.involving.users.in.experiments..Please.answer.according.to.your.personal.beliefs...Experiments.reveal.secrets.about.the.product.strategy
+expinv.statements <- c(
+  "I cannot trust that the experiment results will be correct",
+  "Involving users in experiments is time-consuming",
+  "Our company does not have the needed technical infrastructure",
+  "Users would not like to be part of software experiments",
+  "Users have to be convinced of the benefit before taking part",
+  "Experiments give users false expectations",
+  "Experiments reveal secrets about the product strategy")
+expinv.options <- c("Completely disagree", "Disagree", "Neither disagree or agree", "Agree", "Completely agree", "I don't know")
+
 
 attach(data)
 
@@ -251,3 +332,64 @@ userinf <- data.frame(Statement=factor(rep(userinf.statements, each=length(useri
                         userinf.slf))
 ggplot(data=userinf, aes(x=Statement, y=Rating, fill=Statement)) +
   geom_boxplot() + guides(fill=FALSE) + coord_flip() + scale_x_discrete(limits=c("Myself", "Trainers", "Customer support", "Consultants", "Product and service advocates", "Software architects", "Vaadin Pro Tools developers", "Vaadin Framework developers", "UX designers", "Managers, other", "Product managers")) + theme(axis.text=element_text(size=16))
+
+# 2.4 How often do you use the following ways to get information about users?
+infofreq <- data.frame(Statement=factor(rep(infofreq.statements, each=length(infofreq.O1))),
+                       Rating=c(
+                         infofreq.O1,
+                         infofreq.O2,
+                         infofreq.O3,
+                         infofreq.O4,
+                         infofreq.O5,
+                         infofreq.O6))
+ggplot(data=infofreq, aes(x=Statement, y=Rating, fill=Statement)) +
+  geom_boxplot() + guides(fill=FALSE) + coord_flip() + theme(axis.text=element_text(size=16))
+
+#2.5 Try to remember a situation
+data$userinf.open
+
+# 3.1 Does your company conduct experiments involving the users?
+ggplot(data, aes(x=condexp)) +
+  geom_bar(fill="burlywood1", colour="white") +
+  labs(x="Conducting experiments", y="Frequency") + theme(axis.text=element_text(size=16)) + scale_y_continuous(breaks=c(0,2,6,11), labels = c("0", "2", "6", "11"))
+
+# 3.2 Please describe a typical experiment you have seen or been involved in in your company, including the roles. (Skip this if you have not seen or been involved in any experiments.)
+data$exp.open
+
+# 3.3 Below are three pairs of statements about collecting data for understanding user needs. How much do you agree with each statement?
+understanding <- data.frame(Statement=factor(rep(understanding.statements, each=length(understanding.S1))),
+                            Rating=c(
+                              understanding.S1,
+                              understanding.S2,
+                              understanding.S3,
+                              understanding.S4,
+                              understanding.S5,
+                              understanding.S6))
+ggplot(data=understanding, aes(x=Statement, y=Rating, fill=Statement)) +
+  geom_boxplot() + guides(fill=FALSE) + coord_flip() + scale_x_discrete(limits=c("..we need to measure user behaviour to decide what the software should be like","..users themselves must be actively involved in shaping the software", "..focused data on a specific user action or behaviour is useful", "..rich, detailed data about what users do is useful", "..data should only be collected when there is a known need or assumption", "..data should always be collected because it might be needed later" )) + theme(axis.text=element_text(size=11))
+
+# 4.1 How much do you agree with the following statements regarding notifying users about experiments? Please answer according to your personal beliefs.
+undernotif <- data.frame(Statement=factor(rep(usernotif.statements, each=length(usernotif.S1))),
+                         Rating=c(
+                           usernotif.S1,
+                           usernotif.S2,
+                           usernotif.S3,
+                           usernotif.S4,
+                           usernotif.S5,
+                           usernotif.S6,
+                           usernotif.S7))
+ggplot(data=undernotif, aes(x=Statement, y=Rating, fill=Statement)) +
+  geom_boxplot() + guides(fill=FALSE) + coord_flip() + theme(axis.text=element_text(size=11))
+
+# 4.2 How much do you agree with the following statements about involving users in experiments? Please answer according to your personal beliefs.
+expinv <- data.frame(Statement=factor(rep(expinv.statements, each=length(expinv.S1))),
+                     Rating=c(
+                       expinv.S1,
+                       expinv.S2,
+                       expinv.S3,
+                       expinv.S4,
+                       expinv.S5,
+                       expinv.S6,
+                       expinv.S7))
+ggplot(data=expinv, aes(x=Statement, y=Rating, fill=Statement)) +
+  geom_boxplot() + guides(fill=FALSE) + coord_flip() + theme(axis.text=element_text(size=11))
